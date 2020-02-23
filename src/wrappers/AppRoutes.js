aad77431ed14732bb58d15132619af5ec30634da
colman423 <colman0423@gmail.com>
2020-02-23 21:46:44 +0800

    try figure out why not rendering

--- src/wrappers/AppRoutes.js
+++ src/wrappers/AppRoutes.js
@@ -1,6 +1,9 @@
 import React, { lazy, Suspense } from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Loading from '../components/core/Loading';
+import Home from '../containers/Home'
+import UserProfile from '../containers/UserProfile'
+import About from '../containers/About'
 
 // Helper function to retry lazy import when failed
 // Will try 5 times with 1 sec delay between each attempt
@@ -23,19 +26,19 @@ function retry(fn, retriesLeft = 5, interval = 1000) {
   })
 }
 
-const Home = lazy(() => retry(() => import('../containers/Home')))
-const UserProfile = lazy(() => retry(() => import('../containers/UserProfile')))
-const About = lazy(() => retry(() => import('../containers/About')))
+
+
+
 
 
 function AppRoutes() {
   return (
     <HashRouter basename='/'>
-      <Suspense fallback={<Loading />}>
+      {/* <Suspense fallback={<Loading />}> */}
         <Route exact path="/" component={Home} />
         <Route exact path="/user-profile" component={UserProfile} />
         <Route exact path="/about" component={About} />
-      </Suspense>
+      {/* </Suspense> */}
     </HashRouter>
   );
 }
