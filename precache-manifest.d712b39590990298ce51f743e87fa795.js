93c970fb6a2abfed5c31ea93029a7839b30a97f3
colman423 <colman0423@gmail.com>
2020-02-23 16:58:25 +0000

    Deploying to gh-pages from master 2d3805d0dd0863252c9eb46c16db89bd868d493b

---
+++ precache-manifest.d712b39590990298ce51f743e87fa795.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ce9a2a75356a4e6b1fb5cdd3946dee",
+    "url": "/kai-bot-liff/index.html"
+  },
+  {
+    "revision": "f5e5119d95a067ef1321",
+    "url": "/kai-bot-liff/static/js/0.dd6ee031.chunk.js"
+  },
+  {
+    "revision": "6fc1ead71c7785eea4e2",
+    "url": "/kai-bot-liff/static/js/3.cb20f440.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/kai-bot-liff/static/js/3.cb20f440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fce33c65b99e2c3dbc",
+    "url": "/kai-bot-liff/static/js/4.ec40323e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/kai-bot-liff/static/js/4.ec40323e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc44d196f61550f87857",
+    "url": "/kai-bot-liff/static/js/5.4860e267.chunk.js"
+  },
+  {
+    "revision": "8f474649f5307b19ab45",
+    "url": "/kai-bot-liff/static/js/6.e03fef8f.chunk.js"
+  },
+  {
+    "revision": "30de687fea60481d288a",
+    "url": "/kai-bot-liff/static/js/7.e9f40304.chunk.js"
+  },
+  {
+    "revision": "c958c3276f55831272ed",
+    "url": "/kai-bot-liff/static/js/main.5ebffb4d.chunk.js"
+  },
+  {
+    "revision": "03fd377b633b51b09075",
+    "url": "/kai-bot-liff/static/js/runtime-main.8253c697.js"
+  },
+  {
+    "revision": "984e96366b87259456e7c4b4c8f3449b",
+    "url": "/kai-bot-liff/static/media/mineSweeper.984e9636.svg"
+  },
+  {
+    "revision": "afcc9b05354f8ec60867249010aaed09",
+    "url": "/kai-bot-liff/static/media/user.afcc9b05.svg"
+  }
+]);
