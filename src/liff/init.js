1cbd852f1f60520e951267d62e017326d80e7ad1
colman423 <colman0423@gmail.com>
2020-02-23 20:49:42 +0800

    disabled liff function

--- src/liff/init.js
+++ src/liff/init.js
@@ -3,18 +3,18 @@ import VConsole from 'vconsole'
 var vConsole = new VConsole();
 
 export default function init() {
-  liff.init({
-    liffId: process.env.REACT_APP_LIFF_ID
-  }).then((result) => {
-    console.log("success")
-    console.log("isInClient", liff.isInClient(), "isLoggedIn", liff.isLoggedIn())
-    alert("success")
-    alert("isInClient, "+ liff.isInClient()+ ", isLoggedIn, "+ liff.isLoggedIn())
-    if( !liff.isInClient() && !liff.isLoggedIn() ) {
-      liff.login();
-    }
-  }).catch((err) => {
-    console.log(err.code, err.message);
-    alert(err.code, err.message);
-  });
+  // liff.init({
+  //   liffId: process.env.REACT_APP_LIFF_ID
+  // }).then((result) => {
+  //   console.log("success")
+  //   console.log("isInClient", liff.isInClient(), "isLoggedIn", liff.isLoggedIn())
+  //   alert("success")
+  //   alert("isInClient, "+ liff.isInClient()+ ", isLoggedIn, "+ liff.isLoggedIn())
+  //   if( !liff.isInClient() && !liff.isLoggedIn() ) {
+  //     liff.login();
+  //   }
+  // }).catch((err) => {
+  //   console.log(err.code, err.message);
+  //   alert(err.code, err.message);
+  // });
 }
